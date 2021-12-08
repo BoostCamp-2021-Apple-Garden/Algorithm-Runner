5499c405e1204ef7fe6e43afc11c4c1eb2cd854c
jungwonlee <dev.jungwon.lee@gmail.com>
2021-12-09 00:29:48 +0900

    보석 쇼핑 문제 해결

--- Jungwon/211208/보석 쇼핑.cpp
+++ Jungwon/211208/보석 쇼핑.cpp
@@ -4,58 +4,43 @@
 #include <iostream>
 using namespace std;
 
-vector<int> leftFirst(vector<string> gems, map<string, int> tempMap) {
-    vector<int> v;
-    int left=0, right=gems.size()-1;
-    
-    while(true) {
-        if (tempMap[gems[left]]>1) {
-            tempMap[gems[left]]--;
-            left++;
-        }
-        if (tempMap[gems[right]]>1) {
-            tempMap[gems[right]]--;
-            right--;
-        }
-        if (tempMap[gems[left]]==1&&tempMap[gems[right]]==1) break;
-    }
-    v.push_back(left+1);
-    v.push_back(right+1);
-    return v;
-}
-
-vector<int> rightFirst(vector<string> gems, map<string, int> tempMap) {
-    vector<int> v;
-    int left=0, right=gems.size()-1;
-    
-    while(true) {
-        if (tempMap[gems[right]]>1) {
-            tempMap[gems[right]]--;
-            right--;
-        }
-        if (tempMap[gems[left]]>1) {
-            tempMap[gems[left]]--;
-            left++;
-        }
-        if (tempMap[gems[left]]==1&&tempMap[gems[right]]==1) break;
-    }
-    v.push_back(left+1);
-    v.push_back(right+1);
-    return v;
+bool shouldUpdate(vector<int> v, int left, int right) {
+    if (right-left<v[1]-v[0]) return true;
+    if (right-left==v[1]-v[0]&&left<v[0]) return true;
+    else return false;
 }
 
 vector<int> solution(vector<string> gems) {
     map<string, int> m;
-    vector<int> v1, v2;
-    
+    vector<int> v(2);
+
+    int left=0, right=0;
     for (int i=0; i<gems.size(); i++) {
         string gem = gems[i];
-        if (m.find(gem) == m.end()) m[gem]=1;
-        else m[gem]++;
+        if (m.find(gem) == m.end()) {
+            m[gem]=1;
+            right=i;
+            v[1]=i;
+            v[0]=left;
+        }
+        else {
+            m[gem]++;
+            if (gem==gems[left]) {
+                right=i;
+                while(m[gems[left]]>1) {
+                    m[gems[left]]--;
+                    left++;
+                }
+            }
+            if (shouldUpdate(v, left, right)) {
+                v[0]=left;
+                v[1]=right;
+            }
+        }
     }
-    
-    v1 = leftFirst(gems, m);
-    v2 = rightFirst(gems, m);
-    
-    return v1[1]-v1[0] < v2[1]-v2[0] ? v1 : v2;
+
+    v[0]++;
+    v[1]++;
+
+    return v;
 }
