b6024c77cd89f8c38c6f664cd0eb03bcd0f061bc
minji <966kmj@yonsei.ac.kr>
2021-12-09 18:51:09 +0900

    로마 숫자 만들기 해결

---
+++ Minji/211209/16922.py
@@ -0,0 +1,12 @@
+import sys
+from itertools import combinations_with_replacement
+
+rome_numbers = [1, 5, 10, 50]
+numbers_set = set()
+
+n = int(sys.stdin.readline())
+
+for elem in list(combinations_with_replacement([1, 5, 10, 50], n)):
+    numbers_set.add(sum(list(elem)))
+
+print(len(list(numbers_set)))
