18238ae96149dd7bc2172035dc10c5763118c6d5
jeonyeohun <21500630@handong.edu>
2021-12-16 15:19:58 +0900

    빗물, 부분수열의합 해결

---
+++ YeoHun/211216/14719_빗물.swift
@@ -0,0 +1,187 @@
+
+// 스택으로 하려다가 개고생..
+import Foundation
+
+final class FastIO {
+    private let buffer: [UInt8]
+    private var index: Int = 0
+    private let fhOutput: FileHandle = FileHandle.standardOutput
+    
+    init(fhInput: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fhInput.readToEnd()!) + [UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer {
+            index += 1
+        }
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        if now == 45 {
+            isPositive.toggle(); now = read()
+        }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now - 48)
+            now = read()
+        }
+        return sum * (isPositive ? 1 : -1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        let beginIndex = index-1
+        while now != 10, now != 32, now != 0 {
+            now = read()
+        }
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        let beginIndex = index - 1
+        while now != 10,
+              now != 32,
+              now != 0 {
+            now = read()
+        }
+        return Array(buffer[beginIndex..<(index - 1)])
+    }
+    
+    @inline(__always) func print(_ s: String) {
+        fhOutput.write(s.data(using: .utf8)!)
+    }
+}
+
+let fio = FastIO()
+let _ = fio.readInt()
+let w = fio.readInt()
+var blocks = [Int]()
+for _ in 0..<w {
+    blocks.append(fio.readInt())
+}
+
+func solution() {
+    var start = 0
+    var end = 1
+    var stack = [Int]()
+    var answer = 0
+    
+    while end < w {
+        stack.append(0)
+        while true {
+            if blocks[start] <= blocks[end] || end == w - 1 { break }
+            stack.append(max(blocks[start] - blocks[end], 0))
+            end += 1
+        }
+        stack.append(max(blocks[start] - blocks[end], 0))
+        answer += stack.map({ max(0, $0 - stack.last!) }).reduce(0, +)
+        stack = []
+        start = end
+        end = start + 1
+    }
+    fio.print("\(answer)")
+}
+
+solution()
+
+// 다른 접근
+import Foundation
+
+final class FastIO {
+    private let buffer: [UInt8]
+    private var index: Int = 0
+    private let fhOutput: FileHandle = FileHandle.standardOutput
+    
+    init(fhInput: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fhInput.readToEnd()!) + [UInt8(0)]
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer {
+            index += 1
+        }
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        if now == 45 {
+            isPositive.toggle(); now = read()
+        }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now - 48)
+            now = read()
+        }
+        return sum * (isPositive ? 1 : -1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        let beginIndex = index-1
+        while now != 10, now != 32, now != 0 {
+            now = read()
+        }
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        let beginIndex = index - 1
+        while now != 10,
+              now != 32,
+              now != 0 {
+            now = read()
+        }
+        return Array(buffer[beginIndex..<(index - 1)])
+    }
+    
+    @inline(__always) func print(_ s: String) {
+        fhOutput.write(s.data(using: .utf8)!)
+    }
+}
+
+let fio = FastIO()
+let _ = fio.readInt()
+let w = fio.readInt()
+var blocks = [Int]()
+for _ in 0..<w {
+    blocks.append(fio.readInt())
+}
+
+func solution() {
+    var answer = 0
+    for (index, block) in blocks[1...w-1].enumerated() {
+        let leftMax = blocks[..<(index+1)].max() ?? 0
+        let rightMax = blocks[(index+2)...].max() ?? 0
+        answer += max(0, min(leftMax, rightMax) - block)
+    }
+    print(answer)
+}
+
+solution()
+
