b18bea769646c834484b1d452a110023cba48c84
modyhoon <taehkim@student.42seoul.kr>
2021-12-22 17:34:43 +0900

    taehoon: 이모티콘 해결
    bfs에 대한 직관을 얻었다.
    큐로 넣기 때문에, 최소 시간을 구하는 문제에서
    따로 min() 을 이용하여 비교할 필요가 없다.
    조건이 만족되면 반드시 최초 나오는 depth가 최솟값이기 때문이다.
    bfs를 더 익숙하게 사용하는 연습을 해야겠다.

--- TaeHoon/211220/14226.cpp
+++ TaeHoon/211220/14226.cpp
@@ -4,68 +4,45 @@
 using namespace std;
 
 int targetCount;
-int result = 1 << 30;
-bool visited[10000] = {false, };
-bool visited2[10000] = {false, };
-bool visited3[10000] = {false, };
 
-struct Point {
-	int count, depth, clipboard;
-	Point(int count, int depth, int clipboard) : count(count), depth(depth), clipboard(clipboard) {}
+bool visited[10000][10000] = {false, };
+
+struct Node {
+	public:
+		int currentCount;
+		int clipBoard;
+		int depth;
+		Node(int currentCount, int clipBoard, int depth) : currentCount(currentCount), clipBoard(clipBoard), depth(depth) {}
 };
 
-bool is_in_range(int value) {
-	return value >= 0 && value < 10000;
-}
+int bfs() {
+	queue<Node> q;
 
-void bfs(int start) {
-	queue<Point> q; // currentCount, depth
-
-	q.push(Point(start, 0, 0));
+	q.push(Node(1, 0, 0));
+	visited[1][0] = true;
 	while (!q.empty()) {
-		Point element = q.front();
-		// cout << element.count << ", " << element.depth << ", " << element.clipboard << endl;
+		Node element = q.front();
 		q.pop();
-		if (element.count == targetCount) {
-			result = min(result, element.depth);
+		if (element.currentCount == targetCount) {
+			return element.depth;
 		}
-		for (int i = 0; i < 3; i++) {
-			int newCount;
-			int newClipboard;
-			if (i == 0) {
-				newCount = element.count;
-				newClipboard = element.count;
-				if (is_in_range(newClipboard) && !visited2[newClipboard]) {
-					visited2[newClipboard] = true;
-					q.push(Point(newCount, element.depth + 1, newClipboard));
-				}
-			}
-			else if (i == 1) {
-				if (element.clipboard == 0)
-					continue;
-					// cout << "twice added result : " << element.count << ", " << element.clipboard << endl;
-				newCount = element.count + element.clipboard;
-				newClipboard = element.clipboard;
-				if (is_in_range(newCount) && !visited[newCount]) {
-					visited[newCount] = true;
-					q.push(Point(newCount, element.depth + 1, newClipboard));
-				}
-			}
-			else {
-				newCount = element.count - 1;
-				newClipboard = element.clipboard;
-				if (is_in_range(newCount) && !visited3[newCount]) {
-					visited3[newCount] = true;
-					q.push(Point(newCount, element.depth + 1, newClipboard));
-				}
-			}
+		if (!visited[element.currentCount][element.currentCount]) {
+			visited[element.currentCount][element.currentCount] = true;
+			q.push(Node(element.currentCount, element.currentCount, element.depth + 1));
+		}
+		if (element.clipBoard > 0 && !visited[element.currentCount + element.clipBoard][element.clipBoard]) {
+			visited[element.currentCount + element.clipBoard][element.clipBoard] = true;
+			q.push(Node(element.currentCount + element.clipBoard, element.clipBoard, element.depth + 1));
+		}
+		if (element.currentCount > 0 && !visited[element.currentCount - 1][element.clipBoard]) {
+			visited[element.currentCount - 1][element.clipBoard] = true;
+			q.push(Node(element.currentCount - 1, element.clipBoard, element.depth + 1));
 		}
 	}
+	return 0;
 }
 
 int main(void) {
 	cin >> targetCount;
-
-	bfs(1);
-	cout << result;
+	cout << bfs() << endl;
 }
